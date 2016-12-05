c6d07c66e5336e706961fef9f017f6731eb82a01
Mark Wallace <lotabout@gmail.com>
2016-12-05 21:58:49 +0800

    [install] add script for install configurations

---
+++ install.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+#==============================================================================
+if [[ "$(uname)" == "Darwin" ]]; then
+    OS="Mac"
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    OS="Linux"
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+    OS="MinGW"
+fi
+
+#==============================================================================
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+BACKUP_FILES=(".vim" ".vimrc" ".nvim" ".nvimrc" ".bash_profile" ".bashrc" ".zshrc" ".tmux.conf" ".ideavimrc" ".Xresources" "bin")
+
+# remove old links or backup old files/dirs
+for FILE in "${BACKUP_FILES[@]}"
+do
+    if [[ -L $HOME/$FILE ]]; then
+        rm $HOME/$FILE
+    elif [[ -r $HOME/$FILE ]]; then
+        mv $HOME/$FILE $HOME/$FILE.bak-$(date +"%F-%R")
+    fi
+done
+
+
+AUTO_LINK_FILES=(".vim" ".vimrc" ".nvim" ".nvimrc" ".bash_profile" ".zshrc" ".tmux.conf" ".ideavimrc" ".Xresources" "bin")
+
+# add symlinks
+for FILE in "${AUTO_LINK_FILES[@]}"
+do
+    ln $DIR/$FILE $HOME/$FILE
+done
+
+# Link indivisual ones
+ln $DIR/.bash_profile $HOME/.bashrc
+
+#==============================================================================
+# zsh plugins
+
+mkdir -p $HOME/.zsh
+if [[ ! -d ~/.zsh/zsh-autosuggestions ]];then
+    git clone git://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+fi
+
+#==============================================================================
+# install dependencies
+
+#==============================================================================
+# Post install
+
+# install neovim python support
+if hash pip2 > /dev/null; then
+    pip2 install --user --upgrade neovim
+fi
+
+if hash pip3 > /dev/null; then
+    pip3 install --user --upgrade neovim
+fi
+
+# install vim plugins
+vi -c ":PlugInstall | qa"
