71b7c36893f022bd0136df9b85e0093384026762
Mark Wallace <lotabout@gmail.com>
2018-01-17 15:19:44 +0800

    add tig related configurations

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ fi
 #==============================================================================
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-BACKUP_FILES=(".vim" ".vimrc" ".config/nvim" ".bash_profile" ".bashrc" ".zshrc" ".tmux.conf" ".ideavimrc" ".Xresources" "bin" ".config/ranger")
+BACKUP_FILES=(".vim" ".vimrc" ".config/nvim" ".bash_profile" ".bashrc" ".zshrc" ".tmux.conf" ".ideavimrc" ".Xresources" "bin" ".config/ranger" ".tigrc")
 
 # remove old links or backup old files/dirs
 for FILE in "${BACKUP_FILES[@]}"
@@ -25,7 +25,7 @@ do
 done
 
 
-AUTO_LINK_FILES=(".config/nvim" ".bash_profile" ".zshrc" ".tmux.conf" ".ideavimrc" ".Xresources" "bin" ".config/ranger")
+AUTO_LINK_FILES=(".config/nvim" ".bash_profile" ".zshrc" ".tmux.conf" ".ideavimrc" ".Xresources" "bin" ".config/ranger" ".tigrc")
 
 # add symlinks
 for FILE in "${AUTO_LINK_FILES[@]}"
