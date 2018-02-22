99d641ac102b418aa9578f0d0a8788370184f6d9
Mark Wallace <lotabout@gmail.com>
2018-02-22 18:43:36 +0800

    [ranger] update settings
    1. more commands
        - dolphin to open current selected files in dolphin, for later drag
            and drop
        - sk: select files using sk/fzf
    2. more bindings
    3. devicons
    4. update rifle config

--- .config/ranger/commands.py
+++ .config/ranger/commands.py
@@ -1,23 +1,56 @@
 from ranger.api.commands import Command
 from subprocess import PIPE
+from ranger.core.loader import CommandLoader
 
 class sk(Command):
-    def execute(self):
-        command="fasd -Rdl 2>/dev/null | sk"
-        fzf = self.fm.execute_command(command, stdout=PIPE)
-        stdout, stderr = fzf.communicate()
-        directory = stdout.decode('utf-8').rstrip('\n')
-        self.fm.cd(directory)
-
-class fasd(Command):
     """
-    :fasd
+    :sk_select
 
-    Jump to directory using fasd
+    Find a file using sk.
+
+    With a prefix argument select only directories.
     """
     def execute(self):
         import subprocess
-        arg = self.rest(1)
-        if arg:
-            directory = subprocess.check_output(["fasd", "-d"]+arg.split(), universal_newlines=True).strip()
-            self.fm.cd(directory)
+        import os.path
+        if self.quantifier:
+            # match only directories
+            command="sk --no-multi"
+        else:
+            # match files and directories
+            command="sk --no-multi"
+        sk = self.fm.execute_command(command, stdout=subprocess.PIPE)
+        stdout, stderr = sk.communicate()
+        if sk.returncode == 0:
+            sk_file = os.path.abspath(stdout.decode('utf-8').rstrip('\n'))
+            if os.path.isdir(sk_file):
+                self.fm.cd(sk_file)
+            else:
+                self.fm.select_file(sk_file)
+
+class fasd(Command):
+    """
+    use fasd to select the most frequently visited directory
+    """
+    def execute(self):
+        command="fasd -Rdl 2>/dev/null | sk"
+        sk = self.fm.execute_command(command, stdout=PIPE)
+        stdout, stderr = sk.communicate()
+        directory = stdout.decode('utf-8').rstrip('\n')
+        self.fm.cd(directory)
+
+class dolphin(Command):
+    """open current selected directores/files directly in dolphin"""
+    def execute(self):
+        import os.path
+
+        marked_files = [f.path for f in self.fm.thisdir.get_selection()]
+
+        if not marked_files:
+            return
+
+        if len(marked_files) == 1 and os.path.isdir(marked_files[0]):
+            args = ["dolphin"] + marked_files
+        else:
+            args = ["dolphin", "--select"] + marked_files
+        self.fm.loader.add(CommandLoader(args, "open selected files in dolphin"))
